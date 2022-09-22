f66177c022d1bd978e0f401f52818cb6cdb6f5a5
Tiago - Trabalho <tiago-tmt@hotmail.com>
2022-09-22 16:00:08 -0300

    Primeiro commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Headers() {
+
+
+    return (
+
+        <header className="header">
+
+            <img src="./images/troll-face.png" alt="" className="header--image" />
+
+            <h2 className="header--title">Meme Generator</h2>
+
+            <h4 className="header--project">React Course - Project 3</h4>
+
+        </header>
+
+
+    )
+
+}
