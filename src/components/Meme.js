0dd908daf74395eb84ba2e199165a8cf6819b90b
Tiago 3 <tiago-tmt@hotmail.com>
2022-09-29 08:52:28 -0300

    quarto commit

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,20 +1,32 @@
 import React from "react"
+import memesData from '../memesData'
 
 export default function Meme() {
 
+
+    function getMemeImage() {
+
+        // pegando array do JSON
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+
+        console.log(url)
+
+    }
 
     return (
 
         <main>
 
-            <form className="form">
+            <div className="form">
 
                 <input className="form--input" placeholder="Top text" type="text" />
                 <input className="form--input" placeholder="Bottom text" type="text" />
 
-                <button className="form--button">Get a new meme image</button>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
 
-            </form>
+            </div>
 
         </main>
 
